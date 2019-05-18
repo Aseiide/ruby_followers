271204614fccdcc64c69ac6ed5edf6ea8bf7d7d7
Eiseiide <eiseiide@gmail.com>
2019-05-18 16:39:30 +0900

    アクセスキーを非表示に

--- followers.rb
+++ followers.rb
@@ -1,15 +1,15 @@
 require 'twitter'
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "2A9K5ESmsjjRzQIk8VAtm0ze5"
-  config.consumer_secret     = "w0jHFUG739ZbtAYQ9jSbGVDkRMITU3hRriBd8i3jL22PZs05qx"
-  config.access_token        = "113606541-Tz6QB94DN943uUUztNQYjmYjRnYaC6fL17Jb6wCN"
-  config.access_token_secret = "hHJLfiL9hcwtKIbLfttUnUmfilBRPqrs5pEjaqNwVNjHw"
+  config.consumer_key        = "hogehoge"
+  config.consumer_secret     = "fuu"
+  config.access_token        = "fizz"
+  config.access_token_secret = "buzz"
 end
 
 #フォローしてる人のidを100人取得
-a = client.friend_ids("acalab_07").take(100)
+a = client.friend_ids("fuu").take(100)
 #フォロワーのidを100人取得
-b = client.follower_ids("acalab_07").take(100)
+b = client.follower_ids("hoge").take(100)
 
 p a 
 p b 
